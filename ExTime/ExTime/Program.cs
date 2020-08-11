8397e2cdfafa36263856a2b532d79a05e861bb00
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-11 16:58:36 +0300

    Added PrintCurrentTime function

---
+++ ExTime/ExTime/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time = new Time();
+            time.PrintCurrentTime();
+        }
+    }
+}
---
+++ ExTime/ExTime/Time.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExTime
+{
+    public class Time
+    {
+        public void PrintCurrentTime()
+        {
+            Console.WriteLine(DateTime.Now.ToString("hh:mm:ss.tt"));
+        }
+    }
+}
